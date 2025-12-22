d6a2ca5fc0daa20eb60f1d4b35bcf81b954617ef
aslamstar <hathijastar@gmail.com>
2025-12-22 18:43:30 +0530

    added type module and verified extensions

--- backend/models/admin.js
+++
@@ -1,9 +0,0 @@
-import mongoose from "mongoose";
-
-const adminSchema = new mongoose.Schema({
-  username: { type: String, required: true, unique: true },
-  password: { type: String, required: true }, // bcrypt hashed
-  role: { type: String, default: "admin" }
-}, { timestamps: true });
-
-export default mongoose.model("Admin", adminSchema);
