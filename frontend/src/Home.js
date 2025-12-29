9319603687ee60561dbb0e5cab9219cdc5423a38
aslamstar <hathijastar@gmail.com>
2025-12-29 13:35:42 +0530

    lo

--- frontend/src/Home.js
+++ frontend/src/Home.js
@@ -229,7 +229,7 @@ export default function Home({ isLoggedIn, userEmail }) {
 
           <div>
             <h4>Contact</h4>
-            <div>@KingLinkySupport_Bot</div>
+            <div><a href="@KingLinkySupport_Bot">@KingLinkySupport_Bot</a></div>
             <div style={{ marginTop: 8 }}>© {new Date().getFullYear()} Kinglinky</div>
           </div>
         </div>
