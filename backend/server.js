c796508cc3954d32ef49ed1010e0ceb81aafb218
aslamstar <hathijastar@gmail.com>
2026-01-15 21:07:29 +0530

    '

--- backend/server.js
+++ backend/server.js
@@ -43,6 +43,9 @@ app.use(cors({
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.get("/step-test", (req, res) => {
+  res.send("STEP ROUTE WORKING ")
+})
 app.use(stepRoutes);
 app.use(express.static(path.join(__dirname, "public")));
 
